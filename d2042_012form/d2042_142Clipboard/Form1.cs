5f19f076374e392dfb59ed8da461919c693f8f9c
Mikhail Lapin <mlapin@rambler.ru>
2020-12-17 16:47:37 +0300

    21

---
+++ d2042_012form/d2042_142Clipboard/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace d2042_142Clipboard
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+//            textBox1.Text = Clipboard.GetText();
+            IDataObject dob = Clipboard.GetDataObject();
+            string[] strs = dob.GetFormats();
+            String str2 = "";
+            foreach(string str in strs)
+            {
+                str2 += " " + str;
+            }
+            textBox1.Text = str2;
+
+        }
+    }
+}
