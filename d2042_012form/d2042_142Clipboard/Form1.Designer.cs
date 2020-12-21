8bfb10d889b6e2167f4eb59da3495b0c284cbe07
Mikhail Lapin <mlapin@rambler.ru>
2020-12-21 09:32:00 +0300

    9

--- d2042_012form/d2042_142Clipboard/Form1.Designer.cs
+++ d2042_012form/d2042_142Clipboard/Form1.Designer.cs
@@ -30,31 +30,45 @@
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(24, 33);
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(151, 65);
+            this.textBox1.Size = new System.Drawing.Size(473, 173);
             this.textBox1.TabIndex = 0;
+            this.textBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.textBox1_DragDrop);
+            this.textBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.textBox1_DragEnter);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(24, 202);
+            this.button1.Location = new System.Drawing.Point(12, 200);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "past";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(410, 200);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "copy";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(282, 255);
+            this.ClientSize = new System.Drawing.Size(497, 236);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
@@ -68,6 +82,7 @@
 
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- d2042_012form/d2042_142Clipboard/Form1.cs
+++ d2042_012form/d2042_142Clipboard/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
 
 namespace d2042_142Clipboard
 {
@@ -14,19 +15,128 @@ namespace d2042_142Clipboard
         public Form1()
         {
             InitializeComponent();
+
+            textBox1.AllowDrop = true;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
 //            textBox1.Text = Clipboard.GetText();
-            IDataObject dob = Clipboard.GetDataObject();
-            string[] strs = dob.GetFormats();
-            String str2 = "";
-            foreach(string str in strs)
+            IDataObject iData = Clipboard.GetDataObject();
+
+            if (iData.GetDataPresent(DataFormats.Html))
             {
-                str2 += " " + str;
+                // Yes it is, so display it in a text box.
+                String s1 = "", s2 = "", s3 = ""; 
+                s1 = (String)iData.GetData(DataFormats.Html);
+
+                byte[] bytes = Encoding.Default.GetBytes(s1);
+                s2 = Encoding.UTF8.GetString(bytes);
+
+                int a1 = 0, a2 = 0, a3 = 0, a4 = 0;
+                a1 = s2.IndexOf("href=\"", 0);
+                a1 += 6;
+                a2 = s2.IndexOf("\"", a1);
+                a3 = a2 + 2;
+                a4 = s2.IndexOf("</a>");
+                string sh1 = "", sl1 = "";
+                sl1 = s2.Substring(a1, a2 - a1);
+                sh1 = s2.Substring(a3, a4 - a3);
+                s3 = "[" + sh1 + "](" + sl1 + ")";
+                textBox1.Text = s3;
+
+
+            }else if (iData.GetDataPresent(DataFormats.Text, true))
+            {
+                string s1 = "";
+                s1 = Clipboard.GetText();
+                textBox1.Text = s1;
             }
-            textBox1.Text = str2;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string s1 = textBox1.Text;
+            Clipboard.SetText(s1);
+
+        }
+
+        private void textBox1_DragEnter(object sender, DragEventArgs e)
+        {
+
+            if(e.Data.GetDataPresent(DataFormats.Html, false) == true){
+                e.Effect = DragDropEffects.All;
+ 
+                String s1 = "", s2 = "", s3 = "";
+                s1 = (String)e.Data.GetData(DataFormats.Html);
+
+                byte[] bytes = Encoding.Default.GetBytes(s1);
+                s2 = Encoding.UTF8.GetString(bytes);
+
+                int a1 = 0, a2 = 0, a3 = 0, a4 = 0;
+                a1 = s2.IndexOf("href=\"", 0);
+                a1 += 6;
+                a2 = s2.IndexOf("\"", a1);
+                a3 = a2 + 2;
+                a4 = s2.IndexOf("</a>");
+                string sh1 = "", sl1 = "";
+                sl1 = s2.Substring(a1, a2 - a1);
+                sh1 = s2.Substring(a3, a4 - a3);
+                s3 = "[" + sh1 + "](" + sl1 + ")";
+                textBox1.Text = s3;
+
+            }
+            else if (e.Data.GetDataPresent(DataFormats.FileDrop, false) == true)
+            {
+                e.Effect = DragDropEffects.All;
+                string s1 = "";
+                string[] sl;
+                IDataObject e1 = e.Data;
+                sl = (string[])e1.GetData(DataFormats.FileDrop);
+                foreach (string file in sl)
+                {
+
+                    s1 += Path.GetFileName(file);
+                    s1 += "\r\n";
+                }
+                //s1 = dataToString(e1);
+                textBox1.Text = s1;
+
+
+            } else if (e.Data.GetDataPresent(DataFormats.Text, true) == true)
+            {
+                e.Effect = DragDropEffects.All;
+                string s1 = "", s2 = "";
+
+                IDataObject e1 = e.Data;
+                
+                s1 = e1.GetData(DataFormats.Text).ToString();
+
+                byte[] bytes = Encoding.Default.GetBytes(s1);
+                s2 = Encoding.UTF8.GetString(bytes);
+
+                textBox1.Text = s2;
+            }
+        }
+
+        private string dataToString(IDataObject e1)
+        {
+            string s1 = "";
+            string[] sl;
+
+            sl = e1.GetFormats();
+            foreach (string s in sl)
+            {
+                s1 += s;
+                s1 += "\r\n";
+                s1 += e1.GetData(s);
+                s1 += "\r\n";
+            }
+            return s1;
+        }
+
+        private void textBox1_DragDrop(object sender, DragEventArgs e)
+        {
 
         }
     }
